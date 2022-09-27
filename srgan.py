3adc2b02869a76793622f2a53767e50101125e86
SeHan Choi <shchoi@DI-SOLUTION.CO.KR>
2022-09-27 18:23:45 +0900

    test

--- srgan.py
+++ srgan.py
@@ -1,8 +1,33 @@
+import os
+
 import torch
 import torch.nn as nn
 
+from torch.utils.data import Dataset, DataLoader
+
+import cv2
+
+import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
+
+import torch.optim as optim
+
+from tqdm import tqdm
+
 # reference
 # https://medium.com/analytics-vidhya/super-resolution-gan-srgan-5e10438aec0c
+
+class CFG:
+    
+    dataPath = './data/'
+    device = 'cuda'
+    
+    lr = 3e-4
+    batch_size = 1
+    
+    discLoss = nn.MSELoss()
+    genLoss = nn.MSELoss()
+    # mse로 한번 해봅시다
 
 class Generator(nn.Module):
     def __init__(self):
@@ -40,9 +65,6 @@ class Generator(nn.Module):
         return torch.add(self.bn(self.conv2(x)), skip_data)
     
     def fourth_block(self, x):
-        # print(x.size())
-        # print(self.conv3(x).size())
-        # print(self.ps(self.conv3(x)).size())
         x = self.prelu(self.ps(self.conv3(x)))
         x = self.prelu(self.ps(self.conv3(x)))
         
@@ -50,7 +72,7 @@ class Generator(nn.Module):
         # Rearranges elements in a tensor of shape (B, C*r^2,H,W) -> (B, C, H*r, W*r)
         x = self.conv4(x)
         return x
-    
+
 class Discriminator(nn.Module):
     def __init__(self):
         super().__init__()
@@ -66,7 +88,6 @@ class Discriminator(nn.Module):
         self.leakyrelu = nn.LeakyReLU()
         self.sigmoid = nn.Sigmoid()
         
-        
     def forward(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
@@ -78,7 +99,7 @@ class Discriminator(nn.Module):
         x = self.conv8(x).flatten()
         result = self.MLPblock(x, in_features=x.size(dim=0))
         return result
-    
+
     def CNNblock(self, in_channels, out_channels, kernel_size, padding, stride, bias):
         
         return nn.Sequential(
@@ -94,20 +115,105 @@ class Discriminator(nn.Module):
         x = self.sigmoid(model(x))
         
         return x
+
+class ImageDataset(Dataset):
+    def __init__(self, dataPath):
+        
+        self.dataPath = dataPath
+        self.dataList = []
+        
+        for path in os.listdir(self.dataPath):
+            self.dataList.append(CFG.dataPath+path)
+
+    def __len__(self):
+        return len(self.dataList)
+
+    def __getitem__(self, index):
+        HRimage = cv2.imread(self.dataList[index], cv2.COLOR_BGR2RGB)
+        h, w = HRimage.shape[:2]
+        LRimage = cv2.resize(HRimage, dsize=(round(w/4), round(h/4)))
+        # 이거 사이즈가 맞아야 할듯? 흠... 일단 돌려봅시다.
+
+        HRimage = ToTensorV2()(image=HRimage)
+        LRimage = ToTensorV2()(image=LRimage)
+
+        return HRimage, LRimage
+
+# input = torch.randn(1,3,256,256)
+# Model = Generator()
+# output = Model(input)
+
+# print(f'input size: {input.size()}')
+# print(f'output size: {output.size()}')
+
+
+def train_one_epoch(G_model, D_model, optimizer, dataloader, epoch):
+    G_model.train()
+    D_model.train()
+    
+    dataset_size = 0
+    running_loss = 0
+    
+    discLoss = CFG.discLoss
+    genLoss = CFG.genLoss
+    
+    bar = tqdm(enumerate(dataloader), total=len(dataloader))
+
+    for step, data in bar:
+        # images = data[0].to(device, dtype=torch.float)
+        # labels = data[1].to(device, dtype=torch.long)
+        HRimages = data[0].to(CFG.device)
+        LRimages = data[1].to(CFG.device)
+
+        batch_size = HRimages.size(0)
+
+        # 나중에 dtype 빼보고도 해보기!!! 되는지
+        # Discriminator Loss part
+        G_outputs = G_model(LRimages).to(CFG.device, dtype=torch.float)
+        
+        fakeLabel = D_model(G_outputs).to(CFG.device, dtype=torch.float)
+        realLabel = D_model(HRimages).to(CFG.device, dtype=torch.float)
+
+        d1Loss = torch.mean(discLoss(fakeLabel, torch.zeros_like(fakeLabel, dtype=torch.float)))
+        d2Loss = torch.mean(discLoss(realLabel, torch.ones_like(realLabel, dtype=torch.float)))
+        dLoss = d1Loss+d2Loss
+        d2Loss.backward()
+        d1Loss.backward()
+        
+        # Generator Loss part
+        gLoss = genLoss(fakeLabel, torch.ones_like(fakeLabel))
         
 
+        loss = nn.CrossEntropyLoss()(outputs, labels)
 
-input = torch.randn(1,3,256,256)
-Model = Generator()
-output = Model(input)
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
 
-print(f'input size: {input.size()}')
-print(f'output size: {output.size()}')
+        running_loss += loss.item()*batch_size
+        dataset_size += batch_size
+        epoch_loss = running_loss/dataset_size
 
-
-
-
-
-
+        bar.set_postfix(EPOCH=epoch, TRAIN_LOSS=epoch_loss)
 
 
+
+
+
+if __name__ == "__main__":
+    # os.environ['KMP_DUPLICATE_LIB_OK']= True
+
+    G_Model = Generator().to(CFG.device)
+    D_Model = Discriminator().to(CFG.device)
+    
+    G_optimizer = optim.Adam(G_Model.parameters(), lr=CFG.lr)
+    D_optimizer = optim.Adam(D_Model.parameters(), lr=CFG.lr)
+    # optimizer Adam인지 논문확인
+    
+    train_dataset = ImageDataset(CFG.dataPath)
+    train_loader = DataLoader(train_dataset, shuffle=True, batch_size=CFG.batch_size)
+    
+    bar = tqdm(enumerate(train_loader), total=len(train_loader))
+    
+    for step, data in bar:
+        print(data)
---
+++ test.py
@@ -0,0 +1,7 @@
+import torch
+
+k = torch.randn((1,3,244,244))
+
+m = torch.ones(k)
+
+print(m.size())
